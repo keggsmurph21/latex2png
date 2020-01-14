42da1cf2abcf8970d94b6a4cb0a97afb5e289f25
Kevin Murphy <keggsmurph21@gmail.com>
2020-01-13 16:19:02 -0800

    setup.py: Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="latex2png",
+    author="Kevin Murphy",
+    author_email="kevin@murp.us",
+    version="0.0.1",
+    packages=find_namespace_packages(include="_latex2png"),
+    description="A tool for converting LaTeX snippets to PNG images",
+    install_requires=[],
+    entry_points={"console_scripts": ["latex2png = _latex2png.__main__:main"]},
+)
